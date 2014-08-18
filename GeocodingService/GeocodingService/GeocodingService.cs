094464ed88f5d1375fa78eb4b396cb008c295e74
Özcan Zafer AYAN <ozcanzaferayan@gmail.com>
2014-08-18 22:25:59 +0300

    Sample WindowsNT Service has been added
    I added sample service which coded from Microsoft website:
    http://msdn.microsoft.com/en-us/library/zt39148a(v=vs.110).aspx

---
+++ GeocodingService/GeocodingService/GeocodingService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace GeocodingService
+{
+    public partial class GeocodingService : ServiceBase
+    {
+        public int eventId = 0;
+
+        public GeocodingService(string[] args)
+        {
+            InitializeComponent();
+            string eventSourceName = "GeocodingSource";
+            string logName = "GeocodingLog";
+            if (args.Count() > 0)
+            {
+                 eventSourceName = args[0];
+            }
+            if (args.Count() > 1) 
+            { 
+                logName = args[1]; 
+            }
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists(eventSourceName))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(eventSourceName, logName);
+            }
+            eventLog1.Source = eventSourceName;
+            eventLog1.Log = logName;    
+      
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart");
+            // Set up a timer to trigger every minute.
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 60000; // 60 seconds
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        private void OnTimer(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            // TODO: Insert monitoring activities here.
+            eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In onStop.");
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("In OnContinue.");
+        }  
+
+        
+    }
+}
